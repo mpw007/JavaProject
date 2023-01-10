d23268f5e343afcd4f185c1794ae04717384ca2c
mpw007 <m.wasnik007@gmail.com>
2023-01-10 14:57:52 +0530

    first commit in java02

--- src/Session02/StingConcat.java
+++ src/Session02/StingConcat.java
@@ -4,6 +4,7 @@ public class StingConcat {
 	public static void main(String[] args) {
 		String s1="Well Come";
 		String s2="Java";
+		System.out.println("Milind");
 		System.out.println(s1+s2);
 	}
 
